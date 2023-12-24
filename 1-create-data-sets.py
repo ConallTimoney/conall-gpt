41fb9f45ebf91fc9c22f05f8f085cb8d05ed07fc
Conall Timoney <conalltimoney@gmail.com>
2023-12-24 20:37:07 +0000

    intriduce max length

--- 1-create-data-sets.py
+++ 1-create-data-sets.py
@@ -1,9 +1,22 @@
 from utils.data import create_train_valid_whatsapp, S3_DATA, S3_BUCKET
+from utils.data import create_train_valid_whatsapp
+from transformers import (
+    AutoModelForSeq2SeqLM
+    , DataCollatorForSeq2Seq
+    , AutoTokenizer
+    , Seq2SeqTrainer
+    , Seq2SeqTrainingArguments
+)
+from peft import LoraConfig, get_peft_model, TaskType
+from utils.data import S3_DATA
+
+MODEL_ID = 'NousResearch/Llama-2-7b-hf'
+tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
 
 data = create_train_valid_whatsapp(
     path=S3_DATA
     , train_sender_name='Conall'
-    , prompt_max_length=tokenizer.model_max_length
+    , prompt_max_length=min(1000, tokenizer.model_max_length)
 )
 
 
@@ -12,12 +25,14 @@ def tokenize_data(data_set, tokenizer):
         data_set['text']
         , truncation=True
         , padding=True
+        , max_length=1000
     )
     
     labels = tokenizer(
         data_set['text']
         , truncation=True
         , padding=True
+        , max_length=1000
     )
     
     model_inputs['labels'] = labels['input_ids']
@@ -25,8 +40,18 @@ def tokenize_data(data_set, tokenizer):
     return model_inputs
 
 
-tokenized_train = data['train'].map(lambda data: tokenize_data(data, tokenizer=tokenizer), batched=False, remove_columns=['text', 'label'])
-tokenized_test = data['test_data'].map(lambda data: tokenize_data(data, tokenizer=tokenizer), batched=False, remove_columns=['text', 'label'])
+tokenized_train = data['train'].map(
+    lambda data: tokenize_data(data, tokenizer=tokenizer)
+    , batched=False
+    , remove_columns=['text', 'label']
+)
+
+tokenized_test = data['test_data'].map(
+    lambda data: tokenize_data(data, tokenizer=tokenizer)
+    , batched=False
+    , remove_columns=['text', 'label']
+)
+
 
 tokenized_train.save_to_disk(
     f's3://{S3_BUCKET}/train'
